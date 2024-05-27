a110c3ca07528fe0ea1edd58f36684a874838868
JhonatanDczel <jariasq@unsa.edu.pe>
2024-05-26 19:54:58 -0500

    estructuracion del trabajo

--- contacto.py
+++ contacto.py
@@ -1,8 +1,8 @@
+#Consideraciones de POO en Python: 
+# __init__ es el constructor de la clase, es el primer metodo que se ejecuta al crear un objeto de la clase
+# self es una referencia al objeto que se esta creando
+# __str__ es un metodo que se ejecuta cuando se quiere imprimir el objeto
 class Contacto: 
-  nombre = ""
-  telefono = ""
-  direccion = ""
-  relacion = ""
 
   def __init__(self, nombre, telefono, direccion, relacion):
     self.nombre = nombre
@@ -15,17 +15,24 @@ class Contacto:
 
   def get_name(self):
     return self.nombre
+
   def get_phone(self):
     return self.telefono
+
   def get_adress(self):
     return self.direccion
+
   def get_relation(self):
     return self.relacion
+
   def set_name(self, name):
     self.nombre = name
+
   def set_phone(self, phone):
     self.telefono = phone
+
   def set_adress(self, adress):
-    self.direccion
+    self.direccion = adress
+
   def set_relation(self, relation):
     self.relacion = relation
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from contacto import Contacto
 
+#Consideraciones sobre la implementacion: 
+# __name__ es una variable que se crea en todos los modulos de Python, el valor de esta variable es "__main__" si el modulo se esta ejecutando como un programa principal con "python nombre_modulo.py" o el nombre del modulo si se esta importando
+
 def main():
     return
 
