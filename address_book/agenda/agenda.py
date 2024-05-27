b5e6c15d52e89cf619ca66482a1b2d16228e3fb9
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-27 15:57:20 -0500

    Implementando el método buscar por nombre itema(A)

--- address_book/agenda/agenda.py
+++ address_book/agenda/agenda.py
@@ -21,3 +21,25 @@ class Agenda:
 
   def __str__(self) -> str:
     return '\n'.join([str(contact) for contact in self._contacts])
+  
+  def getContact(self, search):
+    for contact in self._contacts:
+        name = contact.nombre
+        isName = self._match(name.lower(), search.lower())
+        if isName:
+          return "No encontrado\n"
+        else:
+          return contact
+
+  def _match(self, name, search):
+    lenS = len(search)
+    lenN = len(name)
+    j = 0
+
+    for i in range(lenN - lenS):
+      if j < lenS and name[i] == search[j]: 
+        j += 1
+      elif j == lenS - 1:
+        return True
+    return False
+
