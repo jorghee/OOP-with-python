ee6489178b4af18f4efa399d21f1e21b9bb9dad7
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-28 01:07:58 -0500

    Algoritmo KMP para buscar patrones en el string "nombre" e implementanfo el método "guardar_agenda()"
    Usamos el método parsear_a_json() de la clase Contact para que los
    campos del objeto se convierten en diccionarios en python. Este
    diccionario usando el modulo "json" se parsea a un objeto JSON.

--- address_book/agenda/agenda.py
+++ address_book/agenda/agenda.py
@@ -5,7 +5,6 @@ from .contacto import Contacto
 class Agenda:
   def __init__(self, path_data):
     self._contacts = self.recuperar_agenda(path_data)
-    print(self._contacts)
 
   def recuperar_agenda(self, path_data):
     if not (os.path.exists(path_data)):
@@ -13,8 +12,14 @@ class Agenda:
 
     with open(path_data, "r") as data: 
       datos = json.load(data)
-    return [Contacto.parsear(dic) for dic in datos]
+      print("Recuperación exitosa...\n")
+    return [Contacto.parsear_de_json(dic) for dic in datos]
 
+  def guardar_agenda(self):
+    contacts_pars = [contact.parsear_a_json() for contact in self._contacts]
+    with open("./data/data.json", "w", encoding = "utf-8") as save:
+      json.dump(contacts_pars, save, ensure_ascii=False, indent=2)
+      print("Guardacion exitosa...\n")
 
   def __repr__(self) -> str: 
     return self.__str__()
@@ -22,24 +27,50 @@ class Agenda:
   def __str__(self) -> str:
     return '\n'.join([str(contact) for contact in self._contacts])
   
-  def getContact(self, search):
+  def getContact(self, pattern):
     for contact in self._contacts:
         name = contact.nombre
-        isName = self._match(name.lower(), search.lower())
+        isName = self._match(name.lower(), pattern.lower())
         if isName:
-          return "No encontrado\n"
-        else:
           return contact
+    return "No encontrado\n"
 
-  def _match(self, name, search):
-    lenS = len(search)
-    lenN = len(name)
+  # Algoritmo Knuth-Morris-Pratt (KMP)
+  def _match(self, name, pattern):
+    lps = self._compute_lps(pattern)
+    i = 0
     j = 0
 
-    for i in range(lenN - lenS):
-      if j < lenS and name[i] == search[j]: 
+    while i < len(name):
+      if pattern[j] == name[i]:
+        i += 1
         j += 1
-      elif j == lenS - 1:
+
+      if j == len(pattern):
+        j = lps[j - 1]
         return True
+      elif i < len(name) and pattern[j] != name[i]:
+        if j != 0:
+          j = lps[j - 1]
+        else:
+          i += 1
     return False
 
+  # Generación de la funcion π
+  def _compute_lps(self, pattern):
+    lps = [0] * len(pattern)
+    length = 0
+    i = 1
+
+    while i < len(pattern):
+      if pattern[i] == pattern[length]:
+        length += 1
+        lps[i] = length
+        i += 1
+      else:
+        if length != 0:
+          length = lps[length - 1]
+        else:
+          lps[i] = 0
+          i += 1
+    return lps
