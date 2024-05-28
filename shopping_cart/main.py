26f3b3672a3ab9c89de21fda80b357547ca69aa2
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-28 14:26:34 -0500

    Clase main que crea las instancias de ProductStock y agrega algunos productos al carrito de compras

--- shopping_cart/main.py
+++ shopping_cart/main.py
@@ -1,4 +1,21 @@
 from shopping.product_stock import ProductStock
+from shopping.stock_products import StockProducts
+from shopping.shopping_cart import ShoppingCart
 
 class Main:
-  pass
+  @staticmethod
+  def main():
+    stock = StockProducts()
+    stock.add_product(ProductStock("monitor", 500, 100))
+    stock.add_product(ProductStock("telefono", 150, 300))
+    stock.add_product(ProductStock("teclado", 70, 50))
+    stock.add_product(ProductStock("mouse", 50, 50))
+
+    cart = ShoppingCart(stock)
+    cart.add_item("monitor", 2)
+    cart.add_item("telefono", 5)
+    cart.add_item("teclado", 2)
+
+    print("Todo bien, todo correcto...")
+  
+Main.main()
