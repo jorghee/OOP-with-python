927dcc62e5795f4a5a66dd9c2d3eb96b24b81056
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-29 11:19:40 -0500

    Correct indentation

--- address_book/agenda/contacto.py
+++ address_book/agenda/contacto.py
@@ -1,67 +1,65 @@
-# Consideraciones de la sintaxis:
-# __str__ -> Metodo que se ejecuta cuando se imprime el objeto
-# @property -> Decorador que permite acceder a los atributos privados de la clase
-# @atributo.setter -> Decorador que permite modificar los atributos privados de la clase
-# -> Indica el tipo de retorno de la funcion
-# : -> Indica el tipo de dato de un parametro
+""" Consideraciones de la sintaxis:
+  __str__() > Método que se ejecuta cuando se imprime el objeto.
+  @property > Decorador que permite acceder a los atributos privados de la clase.
+  @field.setter > Decorador que permite modificar los atributos privados de la clase.
+  -> > Indica el tipo de retorno de la funcion
+  : str >  Indica el tipo de dato de un parametro
+"""
 
 class Contacto:
-    def __init__(self, nombre: str, telefono: str, direccion: str, relacion: str):
-        self._nombre = nombre
-        self._telefono = telefono
-        self._direccion = direccion
-        self._relacion = relacion
+  def __init__(self, nombre: str, telefono: str, direccion: str, relacion: str):
+    self._nombre = nombre
+    self._telefono = telefono
+    self._direccion = direccion
+    self._relacion = relacion
 
+  def __repr__(self) -> str:
+    return self.__str__()
 
-    def __repr__(self) -> str:
-        return self.__str__()
+  def __str__(self) -> str:
+    return (f"\nNombre:\t\t{self._nombre}\nTelefono:\t{self._telefono}\n"
+            f"Direccion:\t{self._direccion}\nRelacion:\t{self._relacion}\n")
 
-    def __str__(self) -> str:
-        return (f"\nNombre:\t\t{self._nombre}\nTelefono:\t{self._telefono}\n"
-                f"Direccion:\t{self._direccion}\nRelacion:\t{self._relacion}\n")
+  @staticmethod
+  def parsear_de_json(dic):
+    return Contacto(dic["nombre"], dic["telefono"], dic["direccion"], dic["relacion"])
 
-    @staticmethod
-    def parsear_de_json(dic):
-        return Contacto(dic["nombre"], dic["telefono"], dic["direccion"], dic["relacion"])
+  def parsear_a_json(self):
+    return {
+      "nombre": self.nombre,
+      "telefono": self.telefono,
+      "direccion": self.direccion,
+      "relacion": self.relacion
+    }
 
-    def parsear_a_json(self):
-      return {
-        "nombre": self.nombre,
-        "telefono": self.telefono,
-        "direccion": self.direccion,
-        "relacion": self.relacion
-      }
+  @property
+  def nombre(self) -> str:
+    return self._nombre
 
-    @property
-    def nombre(self) -> str:
-        return self._nombre
+  @nombre.setter
+  def nombre(self, value: str) -> None:
+    self._nombre = value
 
-    @nombre.setter
-    def nombre(self, value: str) -> None:
-        self._nombre = value
+  @property
+  def telefono(self) -> str:
+    return self._telefono
 
-    @property
-    def telefono(self) -> str:
-        return self._telefono
+  @telefono.setter
+  def telefono(self, value: str) -> None:
+    self._telefono = value
 
-    @telefono.setter
-    def telefono(self, value: str) -> None:
-        self._telefono = value
+  @property
+  def direccion(self) -> str:
+    return self._direccion
 
-    @property
-    def direccion(self) -> str:
-        return self._direccion
+  @direccion.setter
+  def direccion(self, value: str) -> None:
+    self._direccion = value
 
-    @direccion.setter
-    def direccion(self, value: str) -> None:
-        self._direccion = value
+  @property
+  def relacion(self) -> str:
+    return self._relacion
 
-    @property
-    def relacion(self) -> str:
-        return self._relacion
-
-    @relacion.setter
-    def relacion(self, value: str) -> None:
-        self._relacion = value
-
-
+  @relacion.setter
+  def relacion(self, value: str) -> None:
+    self._relacion = value
